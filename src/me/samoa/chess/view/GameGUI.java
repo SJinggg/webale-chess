7c79fbd46811d419071b87514390ded5882b9460
manyimy <wong.manyi19@gmail.com>
2020-09-08 20:32:23 +0800

    sixth update

--- src/me/samoa/chess/view/GameGUI.java
+++ src/me/samoa/chess/view/GameGUI.java
@@ -11,11 +11,11 @@ public class GameGUI extends JFrame{
   private JButton startButton;
   private JButton saveButton;
   private JButton endButton;
+  private JButton[][] buttons = new JButton[8][7];
+  private Map<Type, Buffered> chessImage = new HashMap<>();
 
   public GameGUI() {
     super("Webale Chess");
-
-    gameManager.getInstance();
 
     setSize(500, 700);
 
@@ -24,14 +24,29 @@ public class GameGUI extends JFrame{
     endButton = new JButton("End");
     saveButton = new JButton("Save");
     
-    menu.add(startButton);
+    menu.add( startButton);
     menu.add(endButton);
     menu.add(saveButton);
 
     super.setJMenuBar(menu);
 
+    JPanel mpanel = new JPanel(new GridLayout(8,7));
+
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 7; j++) {
+        buttons[i][j] = new JButton();
+        buttons[i][j].setName("("+j+","+i+")");
+        buttons[i][j].setText("("+j+","+i+")");
+        buttons[i][j].setBackground(Color.WHITE);
+        buttons[i][j].setBorder(new LineBorder(Color.BLUE));
+        buttons[i][j].addMouseListener(new ButtonListener());
+        mpanel.add(buttons[i][j]); 
+      } // listener add in control e
+    }
+
+    super.add(mpanel);
+
     setVisible(true);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-
   }
 }
